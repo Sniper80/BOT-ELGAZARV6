aef9ae7985e71e65f629a9f1281c902fed5df662
Sniper80 <75849429+Sniper80@users.noreply.github.com>
2024-01-31 22:34:00 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
+global.owner = ['201020182886'] //ur owner number
+global.ownername = "𝐒𝐍𝐈𝐏𝐄𝐑 ༄❤️" //ur owner name
+global.ytname = "telegram: SNIPER" //ur yt chanel name
 global.socialm = "GitHub: ELGAZAR" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.ownernomer = "201020182886" //ur number
+global.premium = ['201020182886'] //ur premium number
+global.botname = '𝐒𝐍𝐈𝐏𝐄𝐑 ༄❤️' //ur bot name
+global.linkz = "https://t.me/EG_SNIPER" //your theme url which will be displayed on whatsapp
+global.websitex = "https://www11.0zz0.com/2024/01/29/08/645122827.gif" //ur website to be displayed
+global.botscript = 'https://t.me/EG_SNIPER' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "𝐒𝐍𝐈𝐏𝐄𝐑 ༄❤️" //ur sticker watermark packname
+global.author = "⍣01020182886⍣" //ur sticker watermark author
+global.wm = "𝐒𝐍𝐈𝐏𝐄𝐑 ༄❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
